bfe441e85a6d68b7e7158495ebc8c969aa21b4be
Zohan <jonexgou@gmail.com>
2024-09-29 14:02:21 -0500

    Allow any host

--- digits_model/digits_model/settings.py
+++ digits_model/digits_model/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-g8c_5l@ecf)ab5dzc4tjlaxcm6nj(&*in8s8^ru0xfylyw1#31
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
